4017d91bb1fa596f6c0d203f9833f22195978427
Patrice Ferlet <metal3d@gmail.com>
2020-03-19 14:37:39 +0100

    Better installation procedure

--- README.md
+++ README.md
@@ -12,14 +12,13 @@ wget -qO- http://bit.ly/team-urgence > team-urgence.sh && bash team-urgence.sh
 ```
 
 The script will install nginx, docker, and docker-compose if there are not already there.
-Then it will create a "team-urgence" user that will be in docker group. It will also have "sudo" rights
-for the time of installation.
+Then it will create a "team-urgence" user that will be in docker group.
 
 Then it will pull mattermost and meet jitsi repository in team-urgence home directory.
 
 It will configure both tools and start them.
 
 Finally, it will create 2 nginx configuration to be able to contact:
-- https://meet.YOUR\_IP.xip.io to make visio conference (like Google meet)
-- https://chat.YOUR\_IP.xip.io to have a team chat system (like Slack or Microsoft Team)
+- https://meet.YOUR_IP.xip.io to make visio conference (like Google meet)
+- https://chat.YOUR_IP.xip.io to have a team chat system (like Slack or Microsoft Team)
 
--- install.sh
+++ install.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
-if [ "${USER}" != "team-urgence" ]; then
-    which sudo >/dev/null && prefix="sudo"
-    
-    if [ "$USER" != "root" ]; then
-        groups | grep sudo >/dev/null
-        if [ "$?" != "0" ]; then
-            echo "You user isn't in sudo group"
-            echo "Please add that user in sudo group with an administator account"
-            echo "with the command: usermod -aG sudo $USER"
-            exit 1
-        fi
-    fi
+which sudo >/dev/null && prefix="sudo"
+prefix="sudo"
+[ ${USER} == "root" ] && prefix=""
 
-    [ ${USER} == "root" ] && prefix=""
-    echo -e "\E[031mPLEASE, NOTE THAT IMPORTANT INFORMATION\E[0m"
-    cat <<EOF
+if [ "$USER" != "root" ]; then
+    groups | grep sudo >/dev/null
+    if [ "$?" != "0" ]; then
+        echo "You user isn't in sudo group"
+        echo "Please add that user in sudo group with an administator account"
+        echo "with the command: usermod -aG sudo $USER"
+        exit 1
+    fi
+fi
+
+
+echo -e "\E[031mPLEASE, NOTE THAT IMPORTANT INFORMATION\E[0m"
+cat <<EOF
 This script is a simple automation to install meet.jit.si and mattermost
 on a server in urgence mode. That means that:
 - you will use the domain name "xip.io" (e.g. meet.your.ip.address.xip.xio)
 - certificates are not validate, so the users will need to accept default certificate
 
 It will install docker and docker-compose if they are not already installed.
+It will install nginx.
 It will write configuration for nginx in /etc/nginx/sites-available/
 It will create a user named team-urgence that will be in "docker" group
 to launch mattermost and meet.jit.si
+Create /opt/mattermost-docker and /opt/docker-jitsi-meet direcotries where
+resides configuration and docker-compose files.
 
 I'm not responsible on you server and how you use the script !
 
@@ -35,25 +39,11 @@ Keep informed on https://github.com/metal3d/team-urgence
 Enjoy !
 Press any key to continue, or CTRL+C to cancel...
 EOF
+read
 
-    read
-
-    $prefix apt update
-    $prefix apt install sudo
-    $prefix useradd -m -s /bin/bash team-urgence
-    $prefix usermod -aG sudo team-urgence
-    $prefix cp team-urgence.sh /home/team-urgence/team-urgence.sh
-    $prefix chown team-urgence /home/team-urgence/team-urgence.sh
-    # and create a password for that user:
-    # with a strong password
-    echo "Give a password for team-urgence user please:"
-    $prefix passwd team-urgence
-    exec sudo -u team-urgence bash -c "bash /home/team-urgence/team-urgence.sh"
-fi
 
 _PUBLICIP=$(wget -qO- http://whatismyip.akamai.com/)
 _PRIVATEIP=$(ip r get 1 | grep -Po '(\d+.){4}' | tail -n1)
-
 
 echo "Choose how to connect your services:"
 echo "1 - With private IP, only users in you local network via xxx.${_PRIVATEIP}.xip.io"
@@ -74,43 +64,55 @@ case $RESP in
         ;;
 esac
 
+# remove blank
+IP=$(echo $IP)
+
 echo "After installation, you will be able to connect:"
 echo "https://meet.${IP}.xip.io to make visioconference"
 echo "http://chat.${IP}.xip.io to chat with team, exchange files..."
 for i in in {1..10}; do
-    printf "\rYou have 10s to cancel by pressing CTRL+C: %1d " "$((10-i))"
+    printf "\rYou've got 10s to cancel by pressing CTRL+C: %1d " "$((10-i))"
     sleep 1
 done
 echo
 
-cd $HOME
-sudo apt update
-sudo apt install ssl-cert git nginx -y
-sudo systemctl enable nginx
-sudo systemctl start nginx
+grep "team-urgence" /etc/password >/dev/null
+if [ "$?" != "0" ]; then
+    echo "===> Create a team-urgence user"
+    $prefix apt update
+    $prefix apt install sudo
+    $prefix useradd -m -s /bin/bash team-urgence
+    # and create a password for that user:
+    # with a strong password
+    echo "===> Give a password for team-urgence user please:"
+    $prefix passwd team-urgence
+fi
+
+# Install nginx
+cd
+$prefix apt update
+$prefix apt install ssl-cert git nginx -y
+$prefix systemctl enable nginx
+$prefix systemctl start nginx
 
 # install docker
 which docker
 if [ "$?" != 0 ]; then
 	wget -O- https://get.docker.com | bash -
 fi
-groups | grep docker || sudo usermod -aG docker ${USER}
-newgrp docker
+$prefix usermod -aG docker team-urgence
 
 # install docker-compose
-which docker-compose
+which docker-compose >/dev/null
 if [ "$?" != 0 ]; then
-	wget https://github.com/docker/compose/releases/download/1.26.0-rc3/docker-compose-Linux-x86_64
-	sudo mv docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
-	sudo chmod +x /usr/local/bin/docker-compose
+	$prefix wget https://github.com/docker/compose/releases/download/1.26.0-rc3/docker-compose-Linux-x86_64 -O /usr/local/bin/docker-compose
+	$prefix chmod +x /usr/local/bin/docker-compose
 fi
 
 # install mattermost
-git clone https://github.com/mattermost/mattermost-docker.git
-cd mattermost-docker
-docker-compose build
+git clone https://github.com/mattermost/mattermost-docker.git /tmp/mattermost-docker
+cd /tmp/mattermost-docker
 mkdir -pv ./volumes/app/mattermost/{data,logs,config,plugins,client-plugins}
-sudo chown -R 2000:2000 ./volumes/app/mattermost/
 cat 1> docker-compose.yml <<EOF
 version: "3"
 
@@ -148,9 +150,16 @@ services:
       - MM_SQLSETTINGS_DATASOURCE=postgres://mmuser:mmuser_password@db:5432/mattermost?sslmode=disable&connect_timeout=10
 EOF
 
-docker-compose up -d
+cd
+$prefix mv /tmp/mattermost-docker /opt/mattermost-docker
+$prefix chown -R team-urgence /opt/mattermost-docker
+sudo -u team-urgence bach -c "cd /opt/mattermost-docker && docker-compose build"
+$prefix chown -R 2000:2000 /opt/mattermost-docker/volumes/app/mattermost/
+sudo -u team-urgence bash -c "cd /opt/mattermost-docker && docker-compose up -d"
 
-cat 1> mattermost.conf <<EOF
+
+## configure nginx
+cat 1> /tmp/mattermost.conf <<EOF
 map \$http_x_forwarded_proto \$proxy_x_forwarded_proto {
   default \$http_x_forwarded_proto;
   ''       \$scheme;
@@ -196,29 +205,209 @@ server {
         proxy_buffers 256 16k;
         proxy_buffer_size 16k;
         proxy_read_timeout 600s;
-	proxy_pass http://127.0.0.1:8000;
+        proxy_pass http://127.0.0.1:8000;
     }
 }
 EOF
-sudo mv mattermost.conf /etc/nginx/sites-available/mattermost.conf
-sudo ln -sf /etc/nginx/sites-available/mattermost.conf /etc/nginx/sites-enabled/mattermost.conf
-sudo nginx -s reload
+$prefix mv /tmp/mattermost.conf /etc/nginx/sites-available/mattermost.conf
+$prefix ln -sf /etc/nginx/sites-available/mattermost.conf /etc/nginx/sites-enabled/mattermost.conf
+$prefix nginx -s reload
 
 
-cd $HOME
-# now install rocketchat
+## Jitsi
+cd 
 grep "chat.${IP}.xip.io" /etc/hosts || echo "127.0.0.1 chat.${IP}.xip.io" | sudo tee -a /etc/hosts
-git clone https://github.com/jitsi/docker-jitsi-meet
-cd docker-jitsi-meet
+
+# clone and configure
+git clone https://github.com/jitsi/docker-jitsi-meet /tmp/docker-jitsi-meet
+cd /tmp/docker-jitsi-meet
+mkdir -p .jitsi-meet-cfg/{web/letsencrypt,transcripts,prosody,jicofo,jvb}
+
 cp env.example .env
 sed -i 's/#DISABLE_HTTPS=1/DISABLE_HTTPS=1/' .env
 sed -i 's,#PUBLIC_URL="https://meet.example.com",#PUBLIC_URL="https://meet.'${IP}'.xip.io",' .env
 sed -i 's/HTTP_PORT=8000/HTTP_PORT=8080/' .env
+sed -i 's,CONFIG=~/.jitsi-meet-cfg,CONFIG=/opt/docker-jitsi-meet/.jitsi-meet-cfg,' .env
 
-mkdir -p ~/.jitsi-meet-cfg/{web/letsencrypt,transcripts,prosody,jicofo,jvb}
-docker-compose up -d
+cat > docker-compose.yml <<EOF
+version: '3'
 
-cat 1> jitsi.conf <<EOF
+services:
+    # Frontend
+    web:
+        image: jitsi/web
+        ports:
+            - '\${HTTP_PORT}:80'
+            - '\${HTTPS_PORT}:443'
+        restart: unless-stopped
+        volumes:
+            - \${CONFIG}/web:/config
+            - \${CONFIG}/web/letsencrypt:/etc/letsencrypt
+            - \${CONFIG}/transcripts:/usr/share/jitsi-meet/transcripts
+        environment:
+            - ENABLE_AUTH
+            - ENABLE_GUESTS
+            - ENABLE_LETSENCRYPT
+            - ENABLE_HTTP_REDIRECT
+            - ENABLE_TRANSCRIPTIONS
+            - DISABLE_HTTPS
+            - JICOFO_AUTH_USER
+            - LETSENCRYPT_DOMAIN
+            - LETSENCRYPT_EMAIL
+            - PUBLIC_URL
+            - XMPP_DOMAIN
+            - XMPP_AUTH_DOMAIN
+            - XMPP_BOSH_URL_BASE
+            - XMPP_GUEST_DOMAIN
+            - XMPP_MUC_DOMAIN
+            - XMPP_RECORDER_DOMAIN
+            - ETHERPAD_URL_BASE
+            - TZ
+            - JIBRI_BREWERY_MUC
+            - JIBRI_PENDING_TIMEOUT
+            - JIBRI_XMPP_USER
+            - JIBRI_XMPP_PASSWORD
+            - JIBRI_RECORDER_USER
+            - JIBRI_RECORDER_PASSWORD
+            - ENABLE_RECORDING
+        networks:
+            meet.jitsi:
+                aliases:
+                    - \${XMPP_DOMAIN}
+
+    # XMPP server
+    prosody:
+        image: jitsi/prosody
+        restart: unless-stopped
+        expose:
+            - '5222'
+            - '5347'
+            - '5280'
+        volumes:
+            - \${CONFIG}/prosody:/config
+        environment:
+            - AUTH_TYPE
+            - ENABLE_AUTH
+            - ENABLE_GUESTS
+            - GLOBAL_MODULES
+            - GLOBAL_CONFIG
+            - LDAP_URL
+            - LDAP_BASE
+            - LDAP_BINDDN
+            - LDAP_BINDPW
+            - LDAP_FILTER
+            - LDAP_AUTH_METHOD
+            - LDAP_VERSION
+            - LDAP_USE_TLS
+            - LDAP_TLS_CIPHERS
+            - LDAP_TLS_CHECK_PEER
+            - LDAP_TLS_CACERT_FILE
+            - LDAP_TLS_CACERT_DIR
+            - LDAP_START_TLS
+            - XMPP_DOMAIN
+            - XMPP_AUTH_DOMAIN
+            - XMPP_GUEST_DOMAIN
+            - XMPP_MUC_DOMAIN
+            - XMPP_INTERNAL_MUC_DOMAIN
+            - XMPP_MODULES
+            - XMPP_MUC_MODULES
+            - XMPP_INTERNAL_MUC_MODULES
+            - XMPP_RECORDER_DOMAIN
+            - JICOFO_COMPONENT_SECRET
+            - JICOFO_AUTH_USER
+            - JICOFO_AUTH_PASSWORD
+            - JVB_AUTH_USER
+            - JVB_AUTH_PASSWORD
+            - JIGASI_XMPP_USER
+            - JIGASI_XMPP_PASSWORD
+            - JIBRI_XMPP_USER
+            - JIBRI_XMPP_PASSWORD
+            - JIBRI_RECORDER_USER
+            - JIBRI_RECORDER_PASSWORD
+            - JWT_APP_ID
+            - JWT_APP_SECRET
+            - JWT_ACCEPTED_ISSUERS
+            - JWT_ACCEPTED_AUDIENCES
+            - JWT_ASAP_KEYSERVER
+            - JWT_ALLOW_EMPTY
+            - JWT_AUTH_TYPE
+            - JWT_TOKEN_AUTH_MODULE
+            - LOG_LEVEL
+            - TZ
+        networks:
+            meet.jitsi:
+                aliases:
+                    - \${XMPP_SERVER}
+
+    # Focus component
+    jicofo:
+        image: jitsi/jicofo
+        restart: unless-stopped
+        volumes:
+            - \${CONFIG}/jicofo:/config
+        environment:
+            - ENABLE_AUTH
+            - XMPP_DOMAIN
+            - XMPP_AUTH_DOMAIN
+            - XMPP_INTERNAL_MUC_DOMAIN
+            - XMPP_SERVER
+            - JICOFO_COMPONENT_SECRET
+            - JICOFO_AUTH_USER
+            - JICOFO_AUTH_PASSWORD
+            - JICOFO_RESERVATION_REST_BASE_URL
+            - JVB_BREWERY_MUC
+            - JIGASI_BREWERY_MUC
+            - JIBRI_BREWERY_MUC
+            - JIBRI_PENDING_TIMEOUT
+            - TZ
+        depends_on:
+            - prosody
+        networks:
+            meet.jitsi:
+
+    # Video bridge
+    jvb:
+        image: jitsi/jvb
+        restart: unless-stopped
+        ports:
+            - '\${JVB_PORT}:\${JVB_PORT}/udp'
+            - '\${JVB_TCP_PORT}:\${JVB_TCP_PORT}'
+        volumes:
+            - \${CONFIG}/jvb:/config
+        environment:
+            - DOCKER_HOST_ADDRESS
+            - XMPP_AUTH_DOMAIN
+            - XMPP_INTERNAL_MUC_DOMAIN
+            - XMPP_SERVER
+            - JVB_AUTH_USER
+            - JVB_AUTH_PASSWORD
+            - JVB_BREWERY_MUC
+            - JVB_PORT
+            - JVB_TCP_HARVESTER_DISABLED
+            - JVB_TCP_PORT
+            - JVB_STUN_SERVERS
+            - JVB_ENABLE_APIS
+            - TZ
+        depends_on:
+            - prosody
+        networks:
+            meet.jitsi:
+
+# Custom network so all services can communicate using a FQDN
+networks:
+    meet.jitsi:
+EOF
+
+
+# move to /opt
+$prefix mv /tmp/docker-jitsi-meet /opt/docker-jitsi-meet
+$prefix chown -R team-urgence /opt/docker-jitsi-meet
+
+cd
+sudo -u team-urgence bash -c "cd /opt/docker-jitsi-meet/ && docker-compose up -d"
+
+## make nginx config
+cat 1> /tmp/jitsi.conf <<EOF
 server {
     listen 80;
     server_name meet.${IP}.xip.io;
@@ -233,19 +422,19 @@ server {
 
     location / {
         proxy_pass http://127.0.0.1:8080;
-	proxy_set_header Host \$http_host;
-	proxy_set_header X-Real-IP \$remote_addr;
-	proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-	proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header Host \$http_host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
     }
 
 }
 EOF
 
-sudo mv jitsi.conf /etc/nginx/sites-available/jitsi.conf
-sudo ln -sf /etc/nginx/sites-available/jitsi.conf /etc/nginx/sites-enabled/jitsi.conf
-sudo nginx -s reload
-sudo gpasswd -d team-urgence sudo
+cd 
+$prefix mv /tmp/jitsi.conf /etc/nginx/sites-available/jitsi.conf
+$prefix ln -sf /etc/nginx/sites-available/jitsi.conf /etc/nginx/sites-enabled/jitsi.conf
+$prefix nginx -s reload
 
 cat <<EOF
 You can now visit:
