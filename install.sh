cd2fc3d203565fd9a236b2502a71a71cb4cce9d1
Patrice Ferlet <metal3d@gmail.com>
2020-03-21 08:45:32 +0100

    Fixup problem with Websocket
    This fix allow the use of rocket.chat mobile application.

--- install.sh
+++ install.sh
@@ -142,10 +142,17 @@ server {
 
     location / {
         proxy_pass http://127.0.0.1:3000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
         proxy_set_header Host \$http_host;
+
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header X-Forward-Proto http;
+        proxy_set_header X-Nginx-Proxy true;
+
+        proxy_redirect off;
     }
 
 }
